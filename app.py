7efc7b43ea5f0257446bc611ac2529de1d5696e2
Tinalchaudhari <156313811+Student-tinalChaudhari@users.noreply.github.com>
2026-09-25 18:35:20 +0530

    app.py

--- app.py
+++ app.py
@@ -1098,7 +1098,4 @@ def logout():
 # ==================================================
 
 if __name__ == "__main__":
-
-    init_db()
-
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
